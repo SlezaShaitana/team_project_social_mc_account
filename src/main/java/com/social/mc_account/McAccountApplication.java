1e2435761124b356a6cb2376c53c677bb07ff6dc
Your Name <you@example.com>
2024-07-15 23:37:15 +0300

    start project

---
+++ src/main/java/com/social/mc_account/McAccountApplication.java
@@ -0,0 +1,13 @@
+package com.social.mc_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/mc_account/McAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.mc_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
