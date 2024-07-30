c710995316ede8c39528f4e43ee7381b91c2a7f6
Daniil Tishin <danatiska5@gmail.com>
2024-07-30 21:54:58 +0300

    add eureka-client

--- src/main/java/com/social/mc_account/McAccountApplication.java
+++ src/main/java/com/social/mc_account/McAccountApplication.java
@@ -3,11 +3,13 @@ package com.social.mc_account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.social.mc_account.dto", "com.social.mc_account.model", "com.social.mc_account.mapper"})
 @EnableJpaRepositories(basePackages = "com.social.mc_account.repository")
+@EnableDiscoveryClient
 public class McAccountApplication {
 
 	public static void main(String[] args) {
