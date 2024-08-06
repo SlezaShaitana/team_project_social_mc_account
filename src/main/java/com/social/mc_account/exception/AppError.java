b0156fd2f10f257439f9d4763febe0cb8badf60c
Daniil Tishin <danatiska5@gmail.com>
2024-08-07 01:37:25 +0300

    add constructors

--- src/main/java/com/social/mc_account/exception/AppError.java
+++ src/main/java/com/social/mc_account/exception/AppError.java
@@ -1,8 +1,12 @@
 package com.social.mc_account.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppError {
     private int statusCode;
     private String message;
