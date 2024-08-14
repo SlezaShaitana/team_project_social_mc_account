8d685e7b1e5fd3ecc25711dec22a1d92160f988d
Артём Свистунов <artemsvist1809@gmail.com>
2024-08-14 10:51:00 +0300

    change JwtValidation url

--- src/main/java/com/social/mc_account/feign/JwtValidation.java
+++ src/main/java/com/social/mc_account/feign/JwtValidation.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "JwtValidation", url = "http://localhost:8083")
+@FeignClient(value = "JwtValidation", url = "http://localhost:8086")
 public interface JwtValidation {
-    @RequestMapping(method = RequestMethod.GET, value = "/validation")
+
+    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/auth/check-validation")
     Boolean validateToken(@RequestParam("token") String token);
+
 }
