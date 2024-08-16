3b8d55635e07a8e5816ec263c686d347deedb9d3
Daniil Tishin <danatiska5@gmail.com>
2024-08-16 16:28:48 +0300

    update

--- src/main/java/com/social/mc_account/mapper/AccountMapper.java
+++ src/main/java/com/social/mc_account/mapper/AccountMapper.java
@@ -3,12 +3,43 @@ package com.social.mc_account.mapper;
 import com.social.mc_account.dto.*;
 import com.social.mc_account.model.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
-    Account toAccountFromAccountMeDto(AccountMeDTO accountMeDTO);
+    @Mapping(source = "first_name", target = "firstName")
+    @Mapping(source = "last_name", target = "lastName")
+    @Mapping(source = "profile_cover", target = "profileCover")
+    @Mapping(source = "status_code", target = "statusCode")
+    @Mapping(source = "reg_date", target = "regDate")
+    @Mapping(source = "birth_date", target = "birthDate")
+    @Mapping(source = "message_permission", target = "messagePermission")
+    @Mapping(source = "last_online_time", target = "lastOnlineTime")
+    @Mapping(source = "is_online", target = "isOnline")
+    @Mapping(source = "is_blocked", target = "isBlocked")
+    @Mapping(source = "emoji_status", target = "emojiStatus")
+    @Mapping(source = "create_on", target = "createdOn")
+    @Mapping(source = "update_on", target = "updatedOn")
+    @Mapping(source = "deletion_timestamp", target = "deletionTimestamp")
     AccountMeDTO toAccountMeDtoForAccount(Account account);
+
+    @Mapping(source = "firstName", target = "first_name")
+    @Mapping(source = "lastName", target = "last_name")
+    @Mapping(source = "profileCover", target = "profile_cover")
+    @Mapping(source = "statusCode", target = "status_code")
+    @Mapping(source = "regDate", target = "reg_date")
+    @Mapping(source = "birthDate", target = "birth_date")
+    @Mapping(source = "messagePermission", target = "message_permission")
+    @Mapping(source = "lastOnlineTime", target = "last_online_time")
+    @Mapping(source = "isOnline", target = "is_online")
+    @Mapping(source = "isBlocked", target = "is_blocked")
+    @Mapping(source = "emojiStatus", target = "emoji_status")
+    @Mapping(source = "createdOn", target = "create_on")
+    @Mapping(source = "updatedOn", target = "update_on")
+    @Mapping(source = "deletionTimestamp", target = "deletion_timestamp")
+    Account toAccountFromAccountMeDto(AccountMeDTO accountMeDTO);
     List<AccountMeDTO> toAccountsMeDtoForAccounts(List<Account> accounts);
     AccountDataDTO toAccountDataDtoFromAccount(Account account);
     List<AccountPageDTO> toPageDtoAccountsFromAccounts(List<Account> accounts);
