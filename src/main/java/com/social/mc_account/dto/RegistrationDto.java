f371e4f0d0382c461baf558b28ed6e3223df8347
Daniil Tishin <danatiska5@gmail.com>
2024-08-16 13:34:39 +0300

    update RegistrationDto

--- src/main/java/com/social/mc_account/dto/RegistrationDto.java
+++ src/main/java/com/social/mc_account/dto/RegistrationDto.java
@@ -1,10 +1,15 @@
 package com.social.mc_account.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class RegistrationDto {
     private UUID uuid;
