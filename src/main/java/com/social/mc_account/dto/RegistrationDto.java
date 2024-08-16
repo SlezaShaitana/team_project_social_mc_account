9dde5736baa6c1f7ffb9971ae6e599f85bf7092e
Daniil Tishin <danatiska5@gmail.com>
2024-08-16 15:40:14 +0300

    update

--- src/main/java/com/social/mc_account/dto/RegistrationDto.java
+++ src/main/java/com/social/mc_account/dto/RegistrationDto.java
@@ -1,5 +1,6 @@
 package com.social.mc_account.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +19,9 @@ public class RegistrationDto {
     private String email;
     private String password1;
     private String password2;
+    @JsonProperty("firstName")
     private String firstName;
+    @JsonProperty("lastName")
     private String lastName;
     private String captchaSecret;
     private Role role = Role.USER;
