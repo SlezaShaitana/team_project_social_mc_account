f6dd8a6f2b02e55a29dcd17d96b7a034366b256e
Daniil Tishin <danatiska5@gmail.com>
2024-08-19 21:51:26 +0300

    update feignClient

--- src/main/java/com/social/mc_account/feign/JwtValidation.java
+++ src/main/java/com/social/mc_account/feign/JwtValidation.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "JwtValidation", url = "http://localhost:8086/api/v1/auth")
+@FeignClient(value = "JwtValidation", url = "http://79.174.80.200:8086/api/v1/auth")
 public interface JwtValidation {
-    @GetMapping("/check-validation") // Используйте @GetMapping и @RequestParam
+    @GetMapping("/check-validation")
     Boolean validateToken(@RequestParam("token") String token);
 }
