78d1edc487e1a1b1c138608700d1b3e02d8468f8
Daniil Tishin <danatiska5@gmail.com>
2024-08-21 15:59:37 +0300

    code review

--- src/main/java/com/social/mc_account/controller/ApiController.java
+++ src/main/java/com/social/mc_account/controller/ApiController.java
@@ -70,7 +70,7 @@ public class ApiController {
     }
 
     @GetMapping("/search")
-    public AccountPageDTO getListAccounts(@RequestParam SearchDTO searchDTO, @RequestParam Page pageable) {
+    public AccountPageDTO getListAccounts(@ModelAttribute SearchDTO searchDTO, @ModelAttribute Page pageable) {
         return accountService.getListAccounts(searchDTO, pageable);
     }
 
