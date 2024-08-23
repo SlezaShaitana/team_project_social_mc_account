295e6a2648386fe2751762bea9012edc2fd4bdd6
GhostASA <artemsvist1809@gmail.com>
2024-08-23 13:41:25 +0400

    test search

--- src/main/java/com/social/mc_account/dto/SearchDTO.java
+++ src/main/java/com/social/mc_account/dto/SearchDTO.java
@@ -1,5 +1,6 @@
 package com.social.mc_account.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import java.util.*;
 
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class SearchDTO {
     private UUID id;
