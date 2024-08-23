60af4109a92a14e2aeb012f190d425010c495053
GhostASA <artemsvist1809@gmail.com>
2024-08-23 13:38:38 +0400

    test search

--- src/main/java/com/social/mc_account/dto/SearchDTO.java
+++ src/main/java/com/social/mc_account/dto/SearchDTO.java
@@ -2,10 +2,13 @@ package com.social.mc_account.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.*;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class SearchDTO {
     private UUID id;
     private boolean isDeleted;
