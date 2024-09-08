f468ba18f5ed34c8673df0356d0a3e53c893cc91
Daniil Tishin <danatiska5@gmail.com>
2024-09-08 18:41:32 +0300

    update search

--- src/main/java/com/social/mc_account/dto/Page.java
+++ src/main/java/com/social/mc_account/dto/Page.java
@@ -3,9 +3,12 @@ package com.social.mc_account.dto;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Page {
     @Min(value = 0, message = "The page value must be greater than or equal to 0.")
